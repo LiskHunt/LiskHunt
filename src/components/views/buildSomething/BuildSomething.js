42db20cbfa487c8755f0e7483f911d41ce28deb1
kaspe <6796782+xunga@users.noreply.github.com>
2017-08-20 21:09:15 +0200

    Added animations for pages

---
+++ src/components/views/buildSomething/BuildSomething.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import AnimationsWrapper from '../../animations-wrapper/AnimationsWrapper'
+import PropTypes from 'prop-types';
+
+class BuildSomething extends Component {
+
+  render() {
+
+    console.log(this);
+    return (
+      <AnimationsWrapper>
+        <div className="">
+          <div className="container has-text-centered">
+            <h1 className="title is-size-1">
+              <span aria-label="nerd" role="img">
+                🤓
+              </span>{' '}
+              Todo:
+            </h1>
+            <h2 className="subtitle">
+              So you are a developer and you want to build something?
+            </h2>
+          </div>
+        </div>
+      </AnimationsWrapper>
+    );
+
+  }
+};
+
+export default BuildSomething;
+
+BuildSomething.contextTypes = {
+  router: PropTypes.object.isRequired,
+};
