bb7f1595c6d909789cc791eaacce2004d8a1ab71
kaspe <6796782+xunga@users.noreply.github.com>
2017-08-20 20:09:46 +0200

    Added BuildSomething component

---
+++ src/components/buildSomething/BuildSomething.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const BuildSomething = ()=>{
+  return(
+    <div className="">
+      <div className="container has-text-centered">
+        <h1 className="title is-size-1">
+          <span aria-label="nerd" role="img">🤓</span> Todo:
+        </h1>
+        <h2 className="subtitle">
+          So you are a developer and you want to build something?
+        </h2>
+        {/*<div className="has-text-left inline-block">*/}
+          {/*<p><span role="img" aria-label="wave">👋🏻</span> New to Lisk? <a className="underlined" >Start here → </a></p>*/}
+          {/*<p><span role="img" aria-label="wave">🔎</span> Looking to browse resources? <a className="underlined">here →</a></p>*/}
+          {/*<p><span role="img" aria-label="wave">🤓</span> Are you a developer? <a className="underlined" >Let's create stuff → </a></p>*/}
+
+        {/*</div>*/}
+      </div>
+    </div>
+  )
+};
+
+export default BuildSomething;
