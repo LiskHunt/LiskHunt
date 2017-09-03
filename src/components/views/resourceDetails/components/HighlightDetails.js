a1ba45dc9a8a100d65e668d0941956af0aab6b20
kaspe <6796782+xunga@users.noreply.github.com>
2017-09-03 23:41:05 +0200

    Improved HighlightDetails

--- src/components/views/resourceDetails/components/HighlightDetails.js
+++ src/components/views/resourceDetails/components/HighlightDetails.js
@@ -3,6 +3,9 @@ import app_settings from '../../../../config';
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import HighlightDetails_mobile from './mobile/Highlight_mobile'
+import HighlightDetails_desktop from './desktop/Highlight_desktop'
+
 import { labels } from '../../../../lib/resources/resources';
 
 class HighlightDetails extends Component {
@@ -37,65 +40,28 @@ class HighlightDetails extends Component {
   render() {
     let resource = this.props.resource;
     return (
-      <div className="container has-text-centered">
-        <nav className="level">
-          <div className="level-item has-text-centered">
-            <div>
-              <p className="heading">Views</p>
-              <p className="title">
-                {this.props.views}
-              </p>
-            </div>
-          </div>
-          <div className="level-item has-text-centered">
-            <div>
-              <p className="heading">Category</p>
-              <span className={"category-label " + this.labelClassName(resource.category)}>
-                <p className="">
-                  #{resource.category}
-                </p>
-              </span>
-            </div>
-          </div>
-          <div className="level-item has-text-centered title-item">
-            <div>
-              <p className="heading">App Name</p>
-              <a className="title">
-                {resource.app_name} <br className="" />
-                <span className="visit-website">
-                  <a
-                    href={resource.app_link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="button is-small is-outlined is-static top20"
-                  >
-                    <i className="fa fa-external-link" aria-hidden="true" />
-                    Visit website{' '}
-                  </a>
-                </span>
-              </a>
-            </div>
-          </div>
-          <div className="level-item has-text-centered">
-            <div>
-              <p className="heading">Author</p>
-              <p className="title">
-                @{resource.author}
-              </p>
-            </div>
-          </div>
-          <div className="level-item has-text-centered">
-            <div>
-              <p className="heading">Likes</p>
-              <p className="title">
-                <a onClick={this.addUpVote}>
-                  {this.props.votes}
-                  {this.displayCaret()}
-                </a>
-              </p>
-            </div>
-          </div>
-        </nav>
+      <div className="">
+
+        <
+          HighlightDetails_desktop
+          resource={resource}
+          addUpVote={this.addUpVote}
+          labelClassName={this.labelClassName}
+          displayCaret={this.displayCaret}
+          votes={this.props.votes}
+          views={this.props.views}
+        />
+
+        <
+          HighlightDetails_mobile
+          resource={resource}
+          addUpVote={this.addUpVote}
+          labelClassName={this.labelClassName}
+          displayCaret={this.displayCaret}
+          votes={this.props.votes}
+          views={this.props.views}
+        />
+
       </div>
     );
   }
