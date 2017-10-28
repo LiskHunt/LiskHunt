d91b4bbbb20c18934109ff239c9a3ea8dfcc7241
kaspe <6796782+xunga@users.noreply.github.com>
2017-10-28 22:52:02 +0200

    Reindented with spaces

--- src/components/views/cheers/Cheers.js
+++ src/components/views/cheers/Cheers.js
@@ -6,74 +6,74 @@ import { goCheers } from "../../router/routes";
 import { setActivePage } from "../../../actions";
 
 class Cheers extends Component {
-	componentWillMount() {
-		this.props.setActivePage(goCheers);
-	}
+  componentWillMount() {
+    this.props.setActivePage(goCheers);
+  }
 
-	render() {
-		return (
-			<AnimationsWrapper>
-				<div className="container has-text-centered column is-half is-offset-one-quarter">
-					<div className="has-text-centered">
-						<h1 className="title is-size-1">
-							<span aria-label="nerd" role="img">
-								🎩
-							</span>
-							<br className="is-hidden-desktop" /> Cheers Mate!
-						</h1>
-					</div>
-					<div className="has-text-centered top20 bottom20">
-						<p className="bottom20">
-							<br />
-							We are very excited to run LiskHunt a directory of Lisk related
-							resources, apps, dapps & tools.
-						</p>
-						<div>
-							If you are enjoying this tool, we would be very glad if you would like to
-							donate to:
-							<br />
-							<strong>17803167062369576486L</strong>
-							<p>
-								Carbonara and Endro are also looking for votes, if you would like to
-								keep our mood high. feel free to vote him :)
-							</p>
-						</div>
+  render() {
+    return (
+      <AnimationsWrapper>
+        <div className="container has-text-centered column is-half is-offset-one-quarter">
+          <div className="has-text-centered">
+            <h1 className="title is-size-1">
+              <span aria-label="nerd" role="img">
+                🎩
+              </span>
+              <br className="is-hidden-desktop" /> Cheers Mate!
+            </h1>
+          </div>
+          <div className="has-text-centered top20 bottom20">
+            <p className="bottom20">
+              <br />
+              We are very excited to run LiskHunt a directory of Lisk related
+              resources, apps, dapps & tools.
+            </p>
+            <div>
+              If you are enjoying this tool, we would be very glad if you would
+              like to donate to:
+              <br />
+              <strong>17803167062369576486L</strong>
+              <p>
+                Carbonara and Endro are also looking for votes, if you would
+                like to keep our mood high. feel free to vote him :)
+              </p>
+            </div>
 
-						<p className="top20">
-							Created by{" "}
-							<a
-								href="https://github.com/xunga"
-								rel="noopener noreferrer"
-								target="_blank"
-								className="link"
-							>
-								@Carbonara
-							</a>{" "}
-							and {" "}
-							<a
-								href="https://github.com/tomploem"
-								rel="noopener noreferrer"
-								target="_blank"
-								className="link"
-							>
-								@Endro
-							</a>.
-						</p>
-					</div>
-				</div>
-			</AnimationsWrapper>
-		);
-	}
+            <p className="top20">
+              Created by{" "}
+              <a
+                href="https://github.com/xunga"
+                rel="noopener noreferrer"
+                target="_blank"
+                className="link"
+              >
+                @Carbonara
+              </a>{" "}
+              and {" "}
+              <a
+                href="https://github.com/tomploem"
+                rel="noopener noreferrer"
+                target="_blank"
+                className="link"
+              >
+                @Endro
+              </a>.
+            </p>
+          </div>
+        </div>
+      </AnimationsWrapper>
+    );
+  }
 }
 
 const mapStateToProps = state => ({});
 
 const mapDispatchToProps = dispatch =>
-	bindActionCreators(
-		{
-			setActivePage: page => setActivePage(page)
-		},
-		dispatch
-	);
+  bindActionCreators(
+    {
+      setActivePage: page => setActivePage(page)
+    },
+    dispatch
+  );
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cheers);
