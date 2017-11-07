d76eafb0a53c7cb17ece0d73c8833c0e9839440a
kaspe <6796782+xunga@users.noreply.github.com>
2017-11-08 00:06:10 +0100

    added placeholder no resources found

--- src/components/views/delegateProfile/components/PlaceholderNoResources.js
+++ src/components/views/delegateProfile/components/PlaceholderNoResources.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import robby from '../img/robot2.png'
 
 const PlaceholderNoResources = () => {
   return (
-    <div className="">
-      <h1>Oh snap!</h1>
-      <h2>Seems like there are no resources from this delegate</h2>
+    <div className="has-text-centered top20">
+      <figure className="image is-128x128 is-centered-image">
+        <img src={robby} alt="sad robot image"/>
+      </figure>
+      <h1 className="title has-text-dark">Oh snap!</h1>
+      <h2 className="subtitle has-text-dark">Seems like there are no resources from this delegate</h2>
     </div>
   );
 
